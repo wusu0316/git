a3678d67da64817220378e94f3edd7200cc55396
wusu0316 <wusu0316@163.com>
2021-09-24 15:06:18 +0800

    update webdriver

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print("test for Jenkins")
+import os
+import time
+from selenium import webdriver
+
+
+
+def test_ota(ip):
+    driver = webdriver.Chrome(executable_path=r"C:\webdriver\chromedriver.exe")
+    try:
+        driver.get('http://{0}:8888/gem/mmx/connect'.format(ip))
+        time.sleep(2)
+    except Exception as e:
+        print(e)
+    finally:
+        driver.quit()
+
+
+
+if __name__ == '__main__':
+    test_ota("10.80.101.10")
+
